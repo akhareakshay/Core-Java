c7646c83970343632601c621317b5859e815e760
Akshay <akhareakshay2@gmail.com>
2021-05-31 10:32:19 +0530

    FileInputStream

--- IOStreams/src/com/akshay/progs/FileInputtStream.java
+++ IOStreams/src/com/akshay/progs/FileInputtStream.java
@@ -9,10 +9,14 @@ public class FileInputtStream {
 
 	public static void main(String[] args) throws IOException {
 
+		// creating FileInputStream class object
 		FileInputStream fis = new FileInputStream("src/com/akshay/res/FileInputStream.txt");
 
 		int i;
 
+		// calling and iterating the read method
+		// here, read method will iterate till the end of the character and then program
+		// will stop execution
 		while ((i = fis.read()) != -1) {
 			System.out.print(i + " -- ");
 			System.out.println((char) i);
