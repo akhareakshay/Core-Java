599185f99110aa61e078413b5dedacb74b5dd84f
Akshay <akhareakshay2@gmail.com>
2021-07-23 17:05:36 +0530

    contains() implemented .java file

---
+++ String Handling/src/com/codewithakshay/Oprtn10_containsTest.java
@@ -0,0 +1,20 @@
+package com.codewithakshay;
+
+public class Oprtn10_containsTest {
+
+	/*
+	 * contains(String s) method returns true if the given parameter string is
+	 * available in the object
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+		String str = "welcome to codewithakshay";
+
+		System.out.println(str.contains("akshay"));
+		System.out.println(str.contains("with"));
+
+	}
+
+}
