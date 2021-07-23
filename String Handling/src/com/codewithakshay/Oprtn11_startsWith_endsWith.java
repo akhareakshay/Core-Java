d042bccda66fce2e7325f16bf6ebfb39880cb115
Akshay <akhareakshay2@gmail.com>
2021-07-23 17:19:12 +0530

    startsWith() endsWith() implemented .java file

---
+++ String Handling/src/com/codewithakshay/Oprtn11_startsWith_endsWith.java
@@ -0,0 +1,25 @@
+package com.codewithakshay;
+
+public class Oprtn11_startsWith_endsWith {
+
+	/*
+	 * startsWith(String s) returns true if the given param string is available at
+	 * the starting position in the string
+	 * 
+	 * endsWith(String s) returns true if the given param string is avaialble at the
+	 * ending position in the string
+	 */
+
+	public static void main(String[] args) {
+
+		String str = "welcome to codewithakshay";
+
+		System.out.println(str.startsWith("wel"));// true
+		System.out.println(str.startsWith("come"));// false
+
+		System.out.println(str.endsWith("akshay"));// true
+		System.out.println(str.endsWith("aks")); // false
+
+	}
+
+}
