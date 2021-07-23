e502113688e45e47c0cffaea1b015ed3f58602ea
Akshay <akhareakshay2@gmail.com>
2021-07-23 19:58:48 +0530

    valueOf() implemented .java

---
+++ String Handling/src/com/codewithakshay/Oprtn17_valueOfTest.java
@@ -0,0 +1,18 @@
+package com.codewithakshay;
+
+public class Oprtn17_valueOfTest {
+
+	/*
+	 * valueOf() method converts the given data type value as parameter into string
+	 */
+
+	public static void main(String[] args) {
+
+		System.out.println(String.valueOf(10));
+		System.out.println(String.valueOf(true));
+		System.out.println(String.valueOf(10.80));
+		System.out.println(String.valueOf('a'));
+
+	}
+
+}
