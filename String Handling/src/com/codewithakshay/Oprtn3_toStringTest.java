9d9de81ca00b764e4f25f8ee491d3327b784e683
Akshay <akhareakshay2@gmail.com>
2021-07-23 14:02:04 +0530

    toString() .java file

--- String Handling/src/com/codewithakshay/Oprtn3_toStringTest.java
+++ String Handling/src/com/codewithakshay/Oprtn3_toStringTest.java
@@ -1,10 +1,37 @@
 package com.codewithakshay;
 
 public class Oprtn3_toStringTest {
+	
+	/*
+	 * toString() implementation
+	 * 
+	 * it returns/gives the content/string data present in the object 
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String s1 = "Akshay";
+		System.out.println("s1 : " + s1); // Akshay
+		System.out.println("s1 : " + s1.toString()); // Akshay
+		System.out.println();
+
+		String s2 = new String("welcome to codewithakshay");
+		System.out.println("s2 : " + s2); // welcome to codewithakshay
+		System.out.println("s2 : " + s2.toString()); // welcome to codewithakshay
+		System.out.println();
+
+		String s3 = null;
+		System.out.println("s3 : " + s3); // null
+		System.out.println("s3 : " + s3.toString()); // nullPointerException
+		System.out.println();
+
+		/*
+		 * String s4; 
+		 * System.out.println("s4 : "+s4);
+		 * System.out.println("s4 : "+s4.toString()); 
+		 * System.out.println();
+		 */		
+		
 	}
 
 }
