4a768abf4e9c82c54cbe9456111bab959da71b15
Akshay <akhareakshay2@gmail.com>
2021-07-23 20:03:20 +0530

    toUpperCase() and toLowerCase() implemented .java file

---
+++ String Handling/src/com/codewithakshay/Oprtn20_toUpperCase_toLowerCaseTest.java
@@ -0,0 +1,22 @@
+package com.codewithakshay;
+
+public class Oprtn20_toUpperCase_toLowerCaseTest {
+
+	/*
+	 * toUpperCase() method converts the given string to upper case
+	 * 
+	 * toLowerCase() method converts the given string to lower case
+	 */
+
+	public static void main(String[] args) {
+
+		String str = "hey coders, welcome to codewithakshay family";
+		System.out.println("Original String : " + str);
+		System.out.println("Converting given String to upper case : " + str.toUpperCase());
+
+		String str2 = "WHATS APP BUDDIES, LETS CODE HARD";
+		System.out.println("Converting given String to lower case : " + str2.toLowerCase());
+
+	}
+
+}
